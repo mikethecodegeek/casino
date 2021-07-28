5d07892e538c980d613ef32f87df596938272339
mike <mikethecodegeek@gmail.com>
2021-07-28 12:17:49 -0700

    calculate hand done

--- backend/routes/api/blackjack.js
+++ backend/routes/api/blackjack.js
@@ -7,11 +7,9 @@ const Deck = require('../api/deck');
 const router = express.Router();
 
 const checkBlackJack = hand => {
-    // console.log(hand)
     let total = 0;;
     let ace = false
     hand.forEach(card => {
-        console.log(card.value.val)
         if (card.value.val == 1) ace = true;
         if (card.value.val <= 10) {
             total+= card.value.val;
@@ -19,10 +17,25 @@ const checkBlackJack = hand => {
             total+=10;
         }
     })
-    console.log(total)
-    console.log(total ==  11 && ace == true)
     return (total == 11 && ace == true) ? true :false;
+}
 
+const calculateHand = hand => {
+    let total = 0;
+    let ace = false;
+    hand.forEach(card => {
+        if (card.value.val == 1) ace = true;
+        if (card.value.val <= 10) {
+            if (card.value.val == 1 && total+11 <=21) {
+                total+=11;
+            } else {
+                total+= card.value.val;
+            }
+        } else {
+            total+=10;
+        }
+    })
+    return total;
 }
 
 router.get(
@@ -43,13 +56,52 @@ router.get(
           currentHand.dealToPlayer(d)
           currentHand.dealToDealer(p)
       }  
-      checkBlackJack(playerHand);
-      return res.json({
-        dealer:dealerHand[0],
-        player: playerHand
-      });
+      const pBlackJack = checkBlackJack(playerHand);
+
+      const dealerValue = dealerHand[0].value.val < 11 ? dealerHand[0].value.val : 10;
+      const playerValue = calculateHand(playerHand);
+
+      if (dealerHand[0].value.val == 1) {
+          return res.json({
+            dealer:dealerHand[0],
+            player: playerHand,
+            blackJack: pBlackJack,
+            insurance: "Would you like insurance?",
+            dealerVal: dealerValue,
+            playerValue: playerValue
+          });
+      } else {
+          return res.json({
+            dealer:dealerHand[0],
+            player: playerHand,
+            blackJack: pBlackJack,
+            dealerVal: dealerValue,
+            playerValue: playerValue
+          });
+      }
+
     }),
   );
+
+  router.get(
+    '/hitme',
+    asyncHandler(async (req, res) => {
+      
+        currentHand.dealToPlayer();
+        const handVal =calculateHand(currentHand.playerHand);
+       
+        return res.json({
+            dealer:dealerHand[0],
+            player: playerHand,
+            playerhandVal: handVal
+        });
+
+    }),
+  );
+
+
+
+
 
 
 module.exports = router;
